22b939643a75f8bd9bb1ff1b0a0c297184a7763a
heisty <thymyriadcolors@gmail.com>
2019-01-15 12:35:06 +0800

    X1010

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    <title>ANIME STREAMING</title>
+  </head>
+
+  <meta keywords="">
+  </meta>
+  <body>
+         <div class="container">
+
+           <div class="row">
+                  <div class="col-4">
+
+                     <div class="box_pic">
+
+                     </div>
+                      
+                  </div>
+
+                  <div class="col-8">
+
+                     <div class="title">
+
+                     </div>
+
+                  </div>
+           </div>
+         	
+         </div>
+  </body>
+</html>
+</html>
